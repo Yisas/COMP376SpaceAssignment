5e941f4c9b9952d6661632939de03174783f051d
Yisas <yisas_0929@hotmail.com>
2016-09-24 20:53:18 -0400

    space music truncated

--- Assets/Scripts/Utility/ButtonController.cs
+++ Assets/Scripts/Utility/ButtonController.cs
@@ -6,6 +6,7 @@ public class ButtonController : MonoBehaviour {
 
     public AudioSource sceneChangeAudiosource;
     public float delayInterval;
+    public bool cursorVisible;
 
     public void LoadScene(string sceneName)
     {
@@ -15,6 +16,7 @@ public class ButtonController : MonoBehaviour {
     private IEnumerator LoadSceneEnumerator(string sceneName)
     {
         sceneChangeAudiosource.Play();
+        Cursor.visible = cursorVisible;
         yield return new WaitForSeconds(delayInterval);
         SceneManager.LoadScene(sceneName);
     }
